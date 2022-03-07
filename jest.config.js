f40dfb2c5ceb3e4002dd71b70f5d04b07e398d10
Silvia Martinez <sil@silvia.com>
2022-03-07 22:34:56 +0100

    Configured test coverage and sonar

--- jest.config.js
+++ jest.config.js
@@ -6,6 +6,15 @@ const createJestConfig = nextJest({
 });
 
 const customJestConfig = {
+  collectCoverageFrom: [
+    "**/*.js",
+    "!**/*.test.js",
+    "!.next/**/*",
+    "!coverage/**/*",
+    "!jest.config.js",
+    "!next.config.js",
+    "!**/_app.js",
+  ],
   // Add more setup options before each test is run
   // setupFilesAfterEnv: ['<rootDir>/jest.setup.js'],
   // if using TypeScript with a baseUrl set to the root directory then you need the below for alias' to work
