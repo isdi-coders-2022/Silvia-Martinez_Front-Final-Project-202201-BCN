301f17878347c14119ecaf5a8ed3877ea7e95400
Silvia Martinez <sil@silvia.com>
2022-03-07 22:54:05 +0100

    Created component Layout

---
+++ components/Layout/Layout.tsx
@@ -0,0 +1,12 @@
+import Head from "next/head";
+
+export const Layout = () => {
+  return (
+    <>
+      <Head>
+        <title>Wallaplop</title>
+        <meta property="og:title" content="Wallaplop" key="title" />
+      </Head>
+    </>
+  );
+};
