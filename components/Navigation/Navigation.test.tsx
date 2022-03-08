bb05e4625a71d4e92e4e471da094185d53dde85f
Silvia Martinez <sil@silvia.com>
2022-03-08 11:30:34 +0100

    Update test navigation

--- components/Navigation/Navigation.test.tsx
+++ components/Navigation/Navigation.test.tsx
@@ -1,10 +1,15 @@
-import { render } from "@testing-library/react";
+import { render, screen } from "@testing-library/react";
+import "@testing-library/jest-dom";
 import { Navigation } from "./Navigation";
 
 describe("Given a navigation component", () => {
   describe("Whent it is invoked", () => {
     test("Then it is rendered text:'inicio'", () => {
       render(<Navigation />);
+
+      const text = screen.getByText("Inicio");
+
+      expect(text).toBeInTheDocument();
     });
   });
 });
