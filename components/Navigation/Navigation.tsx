3b78f21f6193f0ceddf2dcc95141b978a07829c9
Silvia Martinez <sil@silvia.com>
2022-03-08 10:06:31 +0100

    Update navigation component

--- components/Navigation/Navigation.tsx
+++ components/Navigation/Navigation.tsx
@@ -1,10 +1,23 @@
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faHouse, faUser } from "@fortawesome/free-solid-svg-icons";
 import styled from "styled-components";
+import Image from "next/image";
+import logo from "../../public/wallaplop.jpg";
 
 const NavList = styled.div`
-  display: block;
-  background-color: #c4c4c4;
+  display: flex;
+  justify-content: space-between;
+  position: absolute;
+  top: 0;
+  background-color: rgba(196, 196, 196, 0.16);
+  height: 100px;
+  width: 100%;
+`;
+const Logo = styled.div`
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  margin-left: 30px;
 `;
 
 const List = styled.ul`
@@ -24,6 +37,9 @@ const ListItem = styled.li`
 export const Navigation = () => {
   return (
     <NavList>
+      <Logo>
+        <Image src={logo} alt={"logo"} width={50} height={50} />
+      </Logo>
       <List>
         <ListItem>
           <FontAwesomeIcon icon={faHouse} />
