4da88844a1dda5f4bd5743fd42dc6e415e6d365a
Silvia Martinez <sil@silvia.com>
2022-03-09 23:39:04 +0100

    fixed index test with render with provider

--- __test__/index/index.test.tsx
+++ __test__/index/index.test.tsx
@@ -1,10 +1,10 @@
-import { render } from "@testing-library/react";
+import renderWithProviders from "../../jest.setup";
 import Home from "../../pages";
 
 describe("Given a index page", () => {
   describe("When its invoked", () => {
     test("Then it's render", () => {
-      render(<Home />);
+      renderWithProviders(<Home />);
     });
   });
 });
--- jest.setup.js
+++ jest.setup.js
@@ -2,7 +2,20 @@ import "@testing-library/jest-dom/extend-expect";
 import "@testing-library/jest-dom";
 import { server } from "./__mocks__/server/server";
 import "whatwg-fetch";
+import { Provider } from "react-redux";
+import { render } from "@testing-library/react";
+import store from "./redux/store";
 
 beforeAll(() => server.listen());
 afterEach(() => server.resetHandlers());
 afterAll(() => server.close());
+
+const renderWithProviders = (component) => {
+  const Providers = ({ children }) => {
+    return <Provider store={store}>{children}</Provider>;
+  };
+
+  return render(component, { wrapper: Providers });
+};
+
+export default renderWithProviders;
