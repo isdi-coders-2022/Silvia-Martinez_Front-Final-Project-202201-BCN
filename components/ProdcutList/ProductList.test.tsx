3e05107fe382bb76d2cb83f3a4d738393e5e7939
Silvia Martinez <sil@silvia.com>
2022-03-09 13:29:22 +0100

    Added test at ProductList component

---
+++ components/ProdcutList/ProductList.test.tsx
@@ -0,0 +1,25 @@
+import { render, screen } from "@testing-library/react";
+import "@testing-library/jest-dom";
+import ProductList from "./ProductList";
+import { Producto } from "../../types/Producto";
+
+describe("Given a ProdcutList Component", () => {
+  describe("When it's invoked", () => {
+    test("Then it should render", () => {
+      const productos: Producto[] = [
+        {
+          price: 10,
+          title: "silla",
+          description: "silla bonita",
+          category: "mueble",
+        },
+      ];
+
+      render(<ProductList products={productos} />);
+
+      const text = screen.getByRole("heading", { name: "10 €" });
+
+      expect(text).toBeInTheDocument();
+    });
+  });
+});
