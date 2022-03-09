a8690c003f63c03d1f27811bf88736c0f6cdc842
Silvia Martinez <sil@silvia.com>
2022-03-09 19:10:07 +0100

    Added provider at app page

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,15 +1,17 @@
-import "../styles/globals.css";
 import type { AppProps } from "next/app";
 import { Layout } from "../components/Layout/Layout";
-import { config } from "@fortawesome/fontawesome-svg-core";
+
 import "@fortawesome/fontawesome-svg-core/styles.css";
-config.autoAddCss = false;
+import store from "../redux/store";
+import { Provider } from "react-redux";
 
 function MyApp({ Component, pageProps }: AppProps) {
   return (
-    <Layout>
-      <Component {...pageProps} />
-    </Layout>
+    <Provider store={store}>
+      <Layout>
+        <Component {...pageProps} />
+      </Layout>
+    </Provider>
   );
 }
 
