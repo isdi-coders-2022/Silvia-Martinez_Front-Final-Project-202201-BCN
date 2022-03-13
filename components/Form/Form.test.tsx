c421837763724d4f30ea7ac1de9d3fd573b37c84
Silvia Martinez <sil@silvia.com>
2022-03-13 11:53:37 +0100

    Added on dispatch test on Form

--- components/Form/Form.test.tsx
+++ components/Form/Form.test.tsx
@@ -1,6 +1,19 @@
 import Form from "./Form";
 import { screen } from "@testing-library/react";
 import renderWithProviders from "../../jest.setup";
+import { useDispatch } from "react-redux";
+import userEvent from "@testing-library/user-event";
+import { createProductThunk } from "../../redux/thunks/thunks";
+import { Producto } from "../../types/Producto";
+
+const mockDispatch = jest.fn();
+jest.mock("react-redux", () => ({
+  useDispatch: () => mockDispatch,
+}));
+
+jest.mock("../../redux/thunks/thunks", () => ({
+  createProductThunk: jest.fn(),
+}));
 
 describe("Given a Form component", () => {
   describe("When it's rendered", () => {
@@ -24,6 +37,52 @@ describe("Given a Form component", () => {
       const nameInput = screen.queryByPlaceholderText("Nombre del Producto");
 
       expect(nameInput).toBeInTheDocument();
+    });
+  });
+  describe("When it types in the title input", () => {
+    test("Then it should type the product 'Silla'", () => {
+      renderWithProviders(<Form />);
+
+      const title = screen.getByRole("textbox", { name: "Producto:" });
+
+      const product = "Silla";
+
+      userEvent.type(title, product);
+
+      expect(title).toHaveValue(product);
+    });
+
+    test("Then it should call a dispatch action with the product", () => {
+      renderWithProviders(<Form />);
+
+      const title = "Silla";
+      const category = "muebles";
+      const description = "silla de madera";
+
+      const producto: Producto = {
+        _id: "",
+        category: "muebles",
+        description: "silla de madera",
+        picture: "",
+        price: 0,
+        title: "Silla",
+      };
+
+      userEvent.type(screen.getByRole("textbox", { name: "Producto:" }), title);
+
+      userEvent.type(
+        screen.getByRole("textbox", { name: "Descripcion:" }),
+        description
+      );
+      userEvent.type(
+        screen.getByRole("textbox", { name: "Categoria:" }),
+        category
+      );
+
+      userEvent.click(screen.getByRole("button"));
+
+      expect(mockDispatch).toHaveBeenCalled();
+      expect(createProductThunk).toHaveBeenCalledWith(producto);
     });
   });
 });
