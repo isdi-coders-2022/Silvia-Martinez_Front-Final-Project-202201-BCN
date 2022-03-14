a764d6386d0bb4d1f0c4588564773f1332517b77
Silvia Martinez <sil@silvia.com>
2022-03-14 18:28:29 +0100

    Created a real test to index page

--- __test__/index/index.test.tsx
+++ __test__/index/index.test.tsx
@@ -1,10 +1,22 @@
+import { screen } from "@testing-library/react";
 import renderWithProviders from "../../jest.setup";
 import Home from "../../pages";
 
 describe("Given a index page", () => {
   describe("When its invoked", () => {
-    test("Then it's render", () => {
+    test("Then it's render with a list", () => {
       renderWithProviders(<Home />);
+
+      const list = screen.getByRole("list");
+
+      expect(list).toBeInTheDocument();
+    });
+    test("Then it's render with a heading", () => {
+      renderWithProviders(<Home />);
+
+      const text = screen.getByRole("heading", { name: "Cerca de ti" });
+
+      expect(text).toBeInTheDocument();
     });
   });
 });
