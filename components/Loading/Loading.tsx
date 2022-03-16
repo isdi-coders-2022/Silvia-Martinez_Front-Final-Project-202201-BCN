cbb417064247b9cbc70a624ee78727a9f688069f
Silvia Martinez <sil@silvia.com>
2022-03-16 12:35:54 +0100

    Added Loading component

---
+++ components/Loading/Loading.tsx
@@ -0,0 +1,26 @@
+import { PuffLoader } from "react-spinners";
+import styled from "styled-components";
+
+const LoadingContainer = styled.section`
+  display: flex;
+  flex-direction: column;
+  width: 100vw;
+  justify-content: center;
+  align-items: center;
+  p {
+    color: #fd9cca;
+    font-size: 30px;
+    text-align: center;
+  }
+`;
+
+const Loading = (): JSX.Element => {
+  return (
+    <LoadingContainer>
+      <p>Loading...</p>
+      <PuffLoader color={" #fd9cca"} size={80} />
+    </LoadingContainer>
+  );
+};
+
+export default Loading;
