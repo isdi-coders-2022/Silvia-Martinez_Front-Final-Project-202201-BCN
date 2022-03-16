b47fbf710bbb52d87bbcba2ba1238d7d60ff7c0b
Silvia Martinez <sil@silvia.com>
2022-03-16 10:30:19 +0100

    Added container toast

--- components/Layout/Layout.tsx
+++ components/Layout/Layout.tsx
@@ -1,5 +1,7 @@
 import Head from "next/head";
 import { Navigation } from "../Navigation/Navigation";
+import { ToastContainer } from "react-toastify";
+import "react-toastify/dist/ReactToastify.css";
 
 interface Props {
   children: JSX.Element;
@@ -12,6 +14,18 @@ export const Layout = ({ children }: Props) => {
         <title>Wallaplop</title>
       </Head>
       <Navigation />
+
+      <ToastContainer
+        position="bottom-left"
+        autoClose={5000}
+        hideProgressBar={false}
+        newestOnTop={false}
+        closeOnClick
+        rtl={false}
+        pauseOnFocusLoss
+        draggable
+        pauseOnHover
+      />
       {children}
     </>
   );
