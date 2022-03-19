ff83654bd12a83ee0213d7c06b4356c38d0e0d8e
Silvia Martinez <sil@silvia.com>
2022-03-19 17:53:44 +0100

    Added component categorias

---
+++ components/Categorias/Categorias.tsx
@@ -0,0 +1,125 @@
+import {
+  faBasketballBall,
+  faCar,
+  faCouch,
+  faGamepad,
+  faShirt,
+  faTv,
+  faUtensils,
+  faVirusCovid,
+} from "@fortawesome/free-solid-svg-icons";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import Link from "next/link";
+import styled, { StyledComponent } from "styled-components";
+
+const CategoriasStyled: StyledComponent<"div", {}> = styled.div`
+  display: flex;
+  justify-content: space-evenly;
+  height: 70px;
+`;
+
+const List: StyledComponent<"ul", {}> = styled.ul`
+  list-style: none;
+  margin: 0px;
+  padding: 0px;
+  display: flex;
+  flex-wrap: wrap;
+`;
+
+const ListItem: StyledComponent<"li", {}> = styled.li`
+  padding: 10px;
+  margin: 20px;
+  width: 70px;
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+
+  a {
+    display: flex;
+    flex-direction: column;
+    text-decoration: none;
+    color: black;
+  }
+  svg {
+    color: #fd9cca;
+  }
+  p {
+    color: grey;
+    font-size: 12px;
+  }
+  :hover {
+    border-bottom: 1px solid #fd9cca;
+    p {
+      font-size: 16px;
+    }
+  }
+`;
+
+const Categorias = () => {
+  return (
+    <>
+      <CategoriasStyled>
+        <List>
+          <ListItem>
+            <Link href={"/"}>
+              <a>
+                <FontAwesomeIcon icon={faUtensils} />
+                <p>Cocina</p>
+              </a>
+            </Link>
+          </ListItem>
+          <ListItem>
+            <Link href={"/"}>
+              <a>
+                <FontAwesomeIcon icon={faBasketballBall} />
+                <p>Deportes</p>
+              </a>
+            </Link>
+          </ListItem>
+          <ListItem>
+            <Link href={"/"}>
+              <a>
+                <FontAwesomeIcon icon={faTv} />
+                <p>Electrodomesticos</p>
+              </a>
+            </Link>
+          </ListItem>
+          <ListItem>
+            <Link href={"/"}>
+              <a>
+                <FontAwesomeIcon icon={faCouch} />
+                <p>Hogar</p>
+              </a>
+            </Link>
+          </ListItem>
+          <ListItem>
+            <Link href={"/"}>
+              <a>
+                <FontAwesomeIcon icon={faShirt} />
+                <p>Moda</p>
+              </a>
+            </Link>
+          </ListItem>
+          <ListItem>
+            <Link href={"/"}>
+              <a>
+                <FontAwesomeIcon icon={faCar} />
+                <p>Motor</p>
+              </a>
+            </Link>
+          </ListItem>
+          <ListItem>
+            <Link href={"/"}>
+              <a>
+                <FontAwesomeIcon icon={faGamepad} />
+                <p>Videojuegos</p>
+              </a>
+            </Link>
+          </ListItem>
+        </List>
+      </CategoriasStyled>
+    </>
+  );
+};
+
+export default Categorias;
