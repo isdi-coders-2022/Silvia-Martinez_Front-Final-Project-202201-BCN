755ccc0f330b150b4489c0aaed31dcbfcb6236fd
Silvia Martinez <sil@silvia.com>
2022-03-19 12:26:44 +0100

    Created a new reducer

--- redux/reducers/index.ts
+++ redux/reducers/index.ts
@@ -1,9 +1,11 @@
 import { combineReducers } from "redux";
+import productReducer from "./productReducer";
 import productsReducers from "./productsReducers";
 import userReducers from "./userReducer";
 
 const rootReducer = combineReducers({
   products: productsReducers,
+  product: productReducer,
   user: userReducers,
 });
 
