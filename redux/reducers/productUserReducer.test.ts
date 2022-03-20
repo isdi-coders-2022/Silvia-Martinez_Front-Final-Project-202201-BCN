9f9b93c2700281afcc1a151fe1a7bfc9e1a4e335
Silvia Martinez <sil@silvia.com>
2022-03-21 00:35:18 +0100

    Added actions at product user reducer and tested them

--- redux/reducers/productUserReducer.test.ts
+++ redux/reducers/productUserReducer.test.ts
@@ -1,9 +1,10 @@
+import { HYDRATE } from "next-redux-wrapper";
 import { Producto } from "../../types/Producto";
 import actionTypes from "../actions/actionTypes";
 import productsUserReducer from "./productsUserReducer";
 
 describe("Given a product User Reducer", () => {
-  describe("When given a current obejct and action", () => {
+  describe("When given a current object and a load action", () => {
     test("Then should return a  with current objects and actions", () => {
       const productsUser: Producto[] = [
         {
@@ -48,6 +49,186 @@ describe("Given a product User Reducer", () => {
       const newUserProducts = productsUserReducer(currentProducts, action);
 
       expect(newUserProducts).toStrictEqual(productsUser);
+    });
+  });
+
+  describe("When given a delete action", () => {
+    test("Then should return the list minus the corresponding with the id", () => {
+      const currentProducts: Producto[] = [
+        {
+          _id: "1234",
+          price: "10",
+          title: "silla",
+          description: "silla bonita",
+          category: "mueble",
+          picture: "unafoto.jpg",
+          userID: {
+            username: "",
+            name: "Pepe",
+            picture: "",
+            _id: "",
+            password: "",
+            email: "",
+          },
+        },
+        {
+          _id: "1235",
+          price: "10",
+          title: "taburete",
+          description: "taburete feo",
+          category: "mueble",
+          picture: "unafoto.jpg",
+          userID: {
+            username: "",
+            name: "Pepe",
+            picture: "",
+            _id: "",
+            password: "",
+            email: "",
+          },
+        },
+      ];
+
+      const expectedProducts: Producto[] = [
+        {
+          _id: "1235",
+          price: "10",
+          title: "taburete",
+          description: "taburete feo",
+          category: "mueble",
+          picture: "unafoto.jpg",
+          userID: {
+            username: "",
+            name: "Pepe",
+            picture: "",
+            _id: "",
+            password: "",
+            email: "",
+          },
+        },
+      ];
+
+      const action = {
+        type: actionTypes.deleteProduct,
+        id: "1234",
+      };
+
+      const newProducts = productsUserReducer(currentProducts, action);
+
+      expect(newProducts).toEqual(expectedProducts);
+    });
+  });
+  describe("When given a create action", () => {
+    test("Then should return the list with the new product", () => {
+      const currentProducts: Producto[] = [
+        {
+          _id: "1234",
+          price: "10",
+          title: "silla",
+          description: "silla bonita",
+          category: "mueble",
+          picture: "unafoto.jpg",
+          userID: {
+            username: "",
+            name: "Pepe",
+            picture: "",
+            _id: "",
+            password: "",
+            email: "",
+          },
+        },
+      ];
+      const product: Producto[] = [
+        {
+          _id: "1235",
+          price: "10",
+          title: "taburete",
+          description: "taburete feo",
+          category: "mueble",
+          picture: "unafoto.jpg",
+          userID: {
+            username: "",
+            name: "Pepe",
+            picture: "",
+            _id: "",
+            password: "",
+            email: "",
+          },
+        },
+      ];
+
+      const action = {
+        type: actionTypes.createProduct,
+        product,
+      };
+
+      const newProducts = productsUserReducer(currentProducts, action);
+
+      expect(newProducts.length).toEqual(2);
+    });
+  });
+  describe("When given a update action", () => {
+    test("Then should return the list with the updated product", () => {
+      const currentProducts: Producto[] = [
+        {
+          _id: "1234",
+          price: "10",
+          title: "silla",
+          description: "silla bonita",
+          category: "mueble",
+          picture: "unafoto.jpg",
+          userID: {
+            username: "",
+            name: "Pepe",
+            picture: "",
+            _id: "",
+            password: "",
+            email: "",
+          },
+        },
+        {
+          _id: "1235",
+          price: "10",
+          title: "silla",
+          description: "silla bonita",
+          category: "mueble",
+          picture: "unafoto.jpg",
+          userID: {
+            username: "",
+            name: "Pepe",
+            picture: "",
+            _id: "",
+            password: "",
+            email: "",
+          },
+        },
+      ];
+
+      const product: Producto = {
+        _id: "1234",
+        price: "10",
+        title: "silla",
+        description: "silla fea",
+        category: "mueble",
+        picture: "unafoto.jpg",
+        userID: {
+          username: "",
+          name: "Pepe",
+          picture: "",
+          _id: "",
+          password: "",
+          email: "",
+        },
+      };
+
+      const action = {
+        type: actionTypes.updateProduct,
+        product,
+      };
+
+      const updatedProducts = productsUserReducer(currentProducts, action);
+
+      expect(updatedProducts.length).toBe(2);
     });
   });
 });
--- redux/reducers/productsUserReducer.ts
+++ redux/reducers/productsUserReducer.ts
@@ -5,16 +5,33 @@ import { HYDRATE } from "next-redux-wrapper";
 
 const productsUserReducer = (
   currentProducts: Producto[] = [],
-  action: AnyAction = { type: "" }
+  action: AnyAction
 ) => {
   let newProducts: Producto[];
   switch (action.type) {
-    case HYDRATE:
-      newProducts = [...action.payload.productsUser];
-      break;
-
     case actionTypes.loadUserProducts:
       newProducts = [...action.productsUser];
+      break;
+
+    case actionTypes.deleteProduct:
+      newProducts = currentProducts.filter(
+        (product) => product._id !== action.id
+      );
+      break;
+
+    case actionTypes.createProduct:
+      newProducts = [...currentProducts, action.product];
+      break;
+
+    case actionTypes.updateProduct:
+      newProducts = [
+        ...currentProducts.map((product) => {
+          if (product._id === action.product._id) {
+            return action.product;
+          }
+          return product;
+        }),
+      ];
       break;
 
     default:
