e9a905fb6d846333a5ff58f149344b4a3606d2c5
Silvia Martinez <sil@silvia.com>
2022-03-20 17:56:38 +0100

    Added actions at test

--- redux/actions/actionCreator.test.ts
+++ redux/actions/actionCreator.test.ts
@@ -4,7 +4,12 @@ import {
   createProductActions,
   deleteProductActions,
   loadProductsActions,
+  loadProductsUserActions,
+  loadUserActions,
+  loginUserActions,
+  logoutUserActions,
   registerUserActions,
+  updateProductActions,
 } from "./actionCreator";
 
 describe("Given a loadProductsActions", () => {
@@ -18,11 +23,51 @@ describe("Given a loadProductsActions", () => {
           description: "silla bonita",
           category: "mueble",
           picture: "unafoto.jpg",
+          userID: {
+            _id: "",
+            name: "Pepe",
+            username: "Pepito",
+            email: "pepe@pepe.com",
+            password: "1234",
+            picture: "",
+          },
         },
       ];
 
       const expectedAction = { type: "load-products", products: products };
       const action = loadProductsActions(products);
+      expect(action).toEqual(expectedAction);
+    });
+  });
+});
+
+describe("Given a loadProductsActions", () => {
+  describe("When it receives a product", () => {
+    test("Then it should return an action with product inside", () => {
+      const products: Producto[] = [
+        {
+          _id: "124",
+          price: "10",
+          title: "silla",
+          description: "silla bonita",
+          category: "mueble",
+          picture: "unafoto.jpg",
+          userID: {
+            _id: "",
+            name: "Pepe",
+            username: "Pepito",
+            email: "pepe@pepe.com",
+            password: "1234",
+            picture: "",
+          },
+        },
+      ];
+
+      const expectedAction = {
+        type: "load-user-products",
+        productsUser: products,
+      };
+      const action = loadProductsUserActions(products);
       expect(action).toEqual(expectedAction);
     });
   });
@@ -39,6 +84,14 @@ describe("Given a deleteProductAction", () => {
           description: "silla bonita",
           category: "mueble",
           picture: "unafoto.jpg",
+          userID: {
+            _id: "",
+            name: "Pepe",
+            username: "Pepito",
+            email: "pepe@pepe.com",
+            password: "1234",
+            picture: "",
+          },
         },
       ];
       const expectedAction = { type: "delete-product", id: "124" };
@@ -49,7 +102,7 @@ describe("Given a deleteProductAction", () => {
 });
 describe("Given a createProductAction", () => {
   describe("When it receives a product", () => {
-    test("Then it should return an action delete", () => {
+    test("Then it should return an action create", () => {
       const product: Producto = {
         _id: "124",
         price: "10",
@@ -57,9 +110,43 @@ describe("Given a createProductAction", () => {
         description: "silla bonita",
         category: "mueble",
         picture: "unafoto.jpg",
+        userID: {
+          _id: "",
+          name: "Pepe",
+          username: "Pepito",
+          email: "pepe@pepe.com",
+          password: "1234",
+          picture: "",
+        },
       };
       const expectedAction = { type: "create-product", product };
       const action = createProductActions(product);
+      expect(action).toEqual(expectedAction);
+    });
+  });
+});
+
+describe("Given a updateProductAction", () => {
+  describe("When it receives a product", () => {
+    test("Then it should return an action update", () => {
+      const product: Producto = {
+        _id: "124",
+        price: "10",
+        title: "silla",
+        description: "silla bonita",
+        category: "mueble",
+        picture: "unafoto.jpg",
+        userID: {
+          _id: "",
+          name: "Pepe",
+          username: "Pepito",
+          email: "pepe@pepe.com",
+          password: "1234",
+          picture: "",
+        },
+      };
+      const expectedAction = { type: "update-product", product };
+      const action = updateProductActions(product);
       expect(action).toEqual(expectedAction);
     });
   });
@@ -79,6 +166,57 @@ describe("Given a registerUserAction", () => {
 
       const expectedAction = { type: "register-user", user: user };
       const action = registerUserActions(user);
+
+      expect(action).toEqual(expectedAction);
+    });
+  });
+});
+
+describe("Given a loginUserAction", () => {
+  describe("When it receives a user", () => {
+    test("Then it should return an action register", () => {
+      const user: User = {
+        _id: "",
+        name: "Pepe",
+        username: "Pepito",
+        email: "pepe@pepe.com",
+        password: "1234",
+        picture: "",
+      };
+
+      const expectedAction = { type: "login-user", user: user };
+      const action = loginUserActions(user);
+
+      expect(action).toEqual(expectedAction);
+    });
+  });
+});
+
+describe("Given a loadUserAction", () => {
+  describe("When it receives a user", () => {
+    test("Then it should return an action load", () => {
+      const user: User = {
+        _id: "",
+        name: "Pepe",
+        username: "Pepito",
+        email: "pepe@pepe.com",
+        password: "1234",
+        picture: "",
+      };
+
+      const expectedAction = { type: "load-user", user: user };
+      const action = loadUserActions(user);
+
+      expect(action).toEqual(expectedAction);
+    });
+  });
+});
+
+describe("Given a logoutUserAction", () => {
+  describe("When it receives a user", () => {
+    test("Then it should return an action register", () => {
+      const expectedAction = { type: "logout-user" };
+      const action = logoutUserActions();
 
       expect(action).toEqual(expectedAction);
     });
