0131cc63386bff5651c07c00f8645765bb0ad619
Silvia Martinez <sil@silvia.com>
2022-03-20 14:10:38 +0100

    Added tests at thunks

--- __mocks__/handlers/handlers.ts
+++ __mocks__/handlers/handlers.ts
@@ -35,6 +35,21 @@ export const handlers = [
       );
     }
   ),
+  rest.get(
+    `${process.env.NEXT_PUBLIC_WALLAPLOP}products/33`,
+    (req, res, ctx) => {
+      return res(
+        ctx.status(200),
+        ctx.json({
+          _id: "123",
+          price: 10,
+          title: "silla",
+          description: "silla bonita",
+          category: "mueble",
+        })
+      );
+    }
+  ),
   rest.delete(
     `${process.env.NEXT_PUBLIC_WALLAPLOP}products/33`,
     (req, res, ctx) => res(ctx.status(200), ctx.json({}))
