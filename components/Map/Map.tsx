313b6d4b982cd99e781ab984c384685f8eb6a28f
Silvia Martinez <sil@silvia.com>
2022-03-21 12:38:09 +0100

    Updated componente with responsive style

--- components/Map/Map.tsx
+++ components/Map/Map.tsx
@@ -2,8 +2,7 @@ import styled from "styled-components";
 
 const MapContainer = styled.article`
   position: relative;
-  padding-top: 1rem;
-  margin-bottom: 1rem;
+
   .map-responsive iframe {
     left: 0;
     top: 0;
@@ -13,25 +12,39 @@ const MapContainer = styled.article`
   }
   @media screen and (min-device-width: 320px) and (max-width: 768px) {
     overflow: hidden;
-    padding-bottom: 16%;
   }
 `;
 
 interface MapProps {
-  width: string;
-  height: string;
   adress: string;
 }
 
-export const Map = ({ width, height, adress }: MapProps) => {
+export const Map = ({ adress }: MapProps) => {
   return (
     <>
       <MapContainer>
         <iframe
-          width={width}
-          height={height}
+          width={200}
+          height={185}
           frameBorder="0"
-          style={{ border: 0 }}
+          style={{ border: 0, borderRadius: 13 }}
+          src={`https://www.google.com/maps/embed/v1/search?key=${process.env.NEXT_PUBLIC_API_KEY}&q=${adress}&zoom=15`}
+          allowFullScreen
+        ></iframe>
+      </MapContainer>
+    </>
+  );
+};
+
+export const MapDetail = ({ adress }: MapProps) => {
+  return (
+    <>
+      <MapContainer>
+        <iframe
+          width={400}
+          height={200}
+          frameBorder="0"
+          style={{ border: 0, borderRadius: 13 }}
           src={`https://www.google.com/maps/embed/v1/search?key=${process.env.NEXT_PUBLIC_API_KEY}&q=${adress}&zoom=15`}
           allowFullScreen
         ></iframe>
