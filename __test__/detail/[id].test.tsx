24c4f9dc77e30e05253c24ecbc48c6725cbc5f5c
Silvia Martinez <sil@silvia.com>
2022-03-23 17:20:10 +0100

    Added getStaticPAths at test detail page

--- __test__/detail/[id].test.tsx
+++ __test__/detail/[id].test.tsx
@@ -1,4 +1,4 @@
-import { render, screen } from "@testing-library/react";
+import { screen } from "@testing-library/react";
 import { GetStaticPathsContext, GetStaticPropsContext } from "next";
 import { act } from "react-dom/test-utils";
 import renderWithProviders from "../../jest.setup";
@@ -8,23 +8,26 @@ import DetailPage, {
   getStaticProps,
 } from "../../pages/detail/[id]";
 import { wrapper } from "../../redux/store";
-import { Producto } from "../../types/Producto";
 
 describe("Given a detial page", () => {
   describe("When it is invoked", () => {
-    test("It should render a heart icon", () => {
-      const WrappedComponent = wrapper.withRedux(DetailPage);
-      render(<WrappedComponent />);
+    test("It should render a heart icon", async () => {
+      const WrappedComponent = await wrapper.withRedux(DetailPage);
+
+      const context = {
+        params: { id: "33" },
+      };
+
+      await act(async () => {
+        await getStaticProps(context as GetStaticPropsContext);
+        await getStaticPaths(context as GetStaticPathsContext);
+      });
+
+      renderWithProviders(<WrappedComponent />);
+
+      const image = screen.getByRole("img", { name: "like-icon" });
+
+      expect(image).toBeInTheDocument();
     });
   });
 });
-/* test("It should render prodcut price: ' Mira esta producto'", () => {
-      const WrappedComponent = wrapper.withRedux(DetailPage);
-      render(<WrappedComponent />);
-
-      const text = screen.getByRole("heading", {
-        name: "Mira este producto",
-      });
-
-      expect(text).toBeInTheDocument();
-    }); */
